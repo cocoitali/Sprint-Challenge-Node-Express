93427cbd102d6da0ce5a2bfaed94bf254362e75e
cocoitali <cmburatto@gmail.com>
2018-12-31 20:18:12 -0700

    FIX : reorganized index.js and created a seperate file for middleware

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const projectRoutes = require('./projects/projectRoutes')
+const actionRoutes = require('./actions/actionRoutes')
+const mwConfig = require('./data/mwConfig')
+
+const PORT = 5000;
+const server = express();
+
+mwConfig(server)
+
+server.use('/api/projects', projectRoutes)
+server.use('/api/actions', actionRoutes)
+
+
+server.listen(PORT, () => console.log(`API running on port ${PORT}`));
